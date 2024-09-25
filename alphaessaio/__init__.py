ea8539de6654f424d6430a377c9dc6de054b5002
Markus Ziegler <frage_n@web.de>
2024-09-25 20:51:58 +0200

    chore: reworked import

--- alphaessaio/__init__.py
+++ alphaessaio/__init__.py
@@ -1,5 +1,7 @@
 """Import stuff"""
 
-from alphaessaio.client import AlphaEssAPI
+from alphaessaio.client import AlphaEssAPI, AlphaEssAuth
+
+__all__ = [AlphaEssAPI, AlphaEssAuth]
 
 __version__ = "0.0.0"
